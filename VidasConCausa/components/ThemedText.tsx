93cf7f602397d876dd04df7737418b2abc86452f
Javier Eduardo Hurtado Rodriguez <125935868+El-MeGaMer@users.noreply.github.com>
2025-03-03 20:23:41 -0700

    added bold text

--- VidasConCausa/components/ThemedText.tsx
+++ VidasConCausa/components/ThemedText.tsx
@@ -5,7 +5,7 @@ import { useThemeColor } from '@/hooks/useThemeColor';
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+  type?: 'default' | 'title' | 'defaultSemiBold'|'defaultBold' | 'subtitle' | 'link';
 };
 
 export function ThemedText({
@@ -24,6 +24,7 @@ export function ThemedText({
         type === 'default' ? styles.default : undefined,
         type === 'title' ? styles.title : undefined,
         type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
+        type === 'defaultBold' ? styles.defaultBold : undefined,
         type === 'subtitle' ? styles.subtitle : undefined,
         type === 'link' ? styles.link : undefined,
         style,
@@ -43,10 +44,16 @@ const styles = StyleSheet.create({
     lineHeight: 24,
     fontWeight: '600',
   },
+  defaultBold: {
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: 'bold',
+  },
   title: {
     fontSize: 32,
-    fontWeight: 'bold',
+    fontWeight: 'regular',
     lineHeight: 32,
+    fontFamily:'Cocogoose',
   },
   subtitle: {
     fontSize: 20,
